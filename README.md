fe42e18d81118c84cb8429653567eb2429c20f26
Bluexin <bluexin.games@gmail.com>
2015-06-03 12:25:21 +0200

    Little bugfix, updated README.

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ This is a branch of the <a href="http://www.minecraftforum.net/forums/mapping-an
 I used his version 1.5 (for minecraft 1.8) as base (for version 1.0)
 
 
-Planned:
+#Planned:
  * admin-style ui for creative mode (<a href="http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2371404-sword-art-online-ui-mod-continuation-by-mmmgames?comment=110">Link</a>)
  * make the ui open on drag down? (low priority)
+
+
+
+#Legal stuff:
+The code is public. By that I mean anyone willing to read it, both to understand the mechanics of this mod, any rendering in general, or to learn how things work is freely able to.
+But please, don't just rip things off. That won't help you understanding what you do (making the debug/maintaining/... process tedious, and there isn't any merit in claiming other people's work yours.
+The images however can't be re-used. By this I specify you <b>don't have permission</b> to use the textures (or any resource) for your own.
--- src/main/java/com/bluexin/saoui/SAORenderBase.java
+++ src/main/java/com/bluexin/saoui/SAORenderBase.java
@@ -132,6 +132,7 @@ class SAORenderBase extends RenderPlayer {
     }
 
     private void doRenderColorCursor(Minecraft mc, Entity entity, double x, double y, double z, int distance) {
+        if (entity.riddenByEntity != null) return;
         if (SAOOption.LESS_VISUALS.value && !(entity instanceof IMob || SAOMod.getHealth(mc, entity, SAOMod.UNKNOWN_TIME_DELAY) != SAOMod.getMaxHealth(entity)) && !(entity instanceof EntityPlayer)) return;
 
         double d3 = entity.getDistanceSqToEntity(renderManager.livingPlayer);
@@ -212,6 +213,7 @@ class SAORenderBase extends RenderPlayer {
     }
 
     private void doRenderHealthBar(Minecraft mc, Entity entity, double x, double y, double z, float f0, float f1) {
+        if (entity.riddenByEntity != null) return;
         if (entity instanceof EntityPlayer && ((EntityPlayer) entity).capabilities.isCreativeMode) return;
         if (SAOOption.LESS_VISUALS.value && !(entity instanceof IMob || SAOMod.getHealth(mc, entity, SAOMod.UNKNOWN_TIME_DELAY) != SAOMod.getMaxHealth(entity)) && !(entity instanceof EntityPlayer)) return;
         SAOGL.glBindTexture(SAOOption.ORIGINAL_UI.value? SAOResources.entities: SAOResources.entitiesCustom);
